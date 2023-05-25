a954fe51ecb818416cc90d85a5747197ab8c10c5
OrenCS32 <orn.cs1@gmail.com>
2023-05-26 01:46:34 +0300

    added message types

--- server.py
+++ server.py
@@ -7,8 +7,8 @@ SERVER_IP = "0.0.0.0"
 
 SOCKET_ACCEPT_SOCKET_ARG = 0
 
-NOT_MANAGER = 0
 MANAGER = 1
+MUTED = 2
 
 TIMEOUT_TIME = 0.05
 
@@ -25,7 +25,7 @@ PRIVATE_CHAT_TYPE = "5"
 
 CLIENT_SOCKET = 0
 CLIENT_MESSAGE_QUEUE = 1
-CLIENT_IS_OWNER = 2
+CLIENT_STATUS = 2
 clients = dict()
 socket_to_name = dict()
 
@@ -44,7 +44,7 @@ def general_message(name, sock):
 
     client_msg = sock.recv(int(msg_len)).decode()
 
-    output_msg = f"{len(name)}{name}{ALL_CHAT_TYPE}{msg_len}{client_msg}"
+    output_msg = f"{name}: {client_msg}"
 
     for client in clients.values():
         if client[CLIENT_SOCKET] == sock:
@@ -52,7 +52,88 @@ def general_message(name, sock):
         client[CLIENT_MESSAGE_QUEUE].append(output_msg)
 
 
-MESSAGE_TYPES = {ALL_CHAT_TYPE: general_message}
+def private_message(name, sock):
+    name_len: str = sock.recv(NAME_LEN_SIZE).decode()
+
+    if not name_len.isdigit():
+        clients.pop(sock)
+        client_sockets.remove(sock)
+        sock.close()
+        return
+
+    client_name = sock.recv(int(name_len)).decode()
+
+    msg_len: str = sock.recv(MSG_LEN_SIZE).decode()
+
+    if not msg_len.isnumeric():
+        clients.pop(sock)
+        client_sockets.remove(sock)
+        sock.close()
+        return
+
+    client_msg = sock.recv(int(msg_len)).decode()
+
+    output_msg = f"!{name}: {client_msg}"
+
+    clients[client_name][CLIENT_MESSAGE_QUEUE].append(output_msg)
+
+
+def make_owner(name, sock):
+    if not (clients[name][CLIENT_STATUS] & MANAGER):
+        return
+
+    owner_name_len: str = sock.recv(NAME_LEN_SIZE).decode()
+
+    if not owner_name_len.isnumeric():
+        clients.pop(sock)
+        client_sockets.remove(sock)
+        sock.close()
+        return
+
+    owner_name = sock.recv(int(owner_name_len)).decode()
+
+    clients[owner_name][CLIENT_STATUS] |= MANAGER
+
+
+def mute_user(name, sock):
+    if not (clients[name][CLIENT_STATUS] & MANAGER):
+        return
+
+    client_name_len: str = sock.recv(NAME_LEN_SIZE).decode()
+
+    if not client_name_len.isnumeric():
+        clients.pop(sock)
+        client_sockets.remove(sock)
+        sock.close()
+        return
+
+    client_name = sock.recv(int(client_name_len)).decode()
+
+    clients[client_name][CLIENT_STATUS] |= MUTED
+
+
+def kick_user(name, sock):
+    if not (clients[name][CLIENT_STATUS] & MANAGER):
+        return
+
+    client_name_len: str = sock.recv(NAME_LEN_SIZE).decode()
+
+    if not client_name_len.isnumeric():
+        clients.pop(sock)
+        client_sockets.remove(sock)
+        sock.close()
+        return
+
+    client_name = sock.recv(int(client_name_len)).decode()
+
+    client_socket = clients[client_name][CLIENT_SOCKET]
+    clients.pop(client_name)
+    socket_to_name.pop(client_socket)
+    client_sockets.remove(client_socket)
+
+
+MESSAGE_TYPES = {ALL_CHAT_TYPE: general_message, MAKE_OWNER_TYPE: make_owner,
+                 KICK_TYPE: kick_user, MUTE_TYPE: mute_user, PRIVATE_CHAT_TYPE: private_message}
 
 
 def manage_new_clients():
@@ -71,7 +152,7 @@ def manage_new_clients():
 
         name = current_socket.recv(int(name_len)).decode()
         non_named_sockets.remove(current_socket)
-        clients[name] = [current_socket, [], NOT_MANAGER]
+        clients[name] = [current_socket, [], 0]
         socket_to_name[current_socket] = name
         client_sockets.add(current_socket)
         print(f"Added {name}")
