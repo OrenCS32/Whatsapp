b2e3f04f045f911697fab2663cc03b784a5192b4
ItayElf <thh123321@gmail.com>
2023-05-26 10:05:42 +0300

    Feat: added client example

--- client.py
+++ client.py
@@ -1,2 +1,15 @@
 import socket
 
+from network import login, send_chat_message
+
+client = socket.socket()
+
+client.connect(("127.0.0.1", 5555))
+
+username = input("username: ")
+login(client, username)
+
+send_chat_message(client, username, "hello there")
+
+while True:
+    print(client.recv(1024))
